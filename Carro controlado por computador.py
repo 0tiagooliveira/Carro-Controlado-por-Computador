72b68825c5f00b966f6abf97acf7ec7b817e4bec
0tiagooliveira <123430824+0tiagooliveira@users.noreply.github.com>
2023-03-01 16:35:58 -0300

    Create Carro controlado por computador.py

---
+++ Carro controlado por computador.py
@@ -0,0 +1,27 @@
+# Condições simples e Condições compostas
+    # Exemplo do professor
+    # Carro em um ponto de destino
+    # este carro é controlado por computador e só aceita comandos como
+    # vire para esquerda, direita e pare.
+
+# Exemplo 01 carro é nosso objeto
+
+#carro.siga()
+    # objeto seguir em fente
+#carro.esquerda()
+    # objeto vire para esquerda
+#carro.direita()
+    # objeto vire para direita
+#carro.pare()
+    # objeto pare
+
+# Vamos aprender a usar o se e senão
+     # if = se
+     # senão = else
+        #exemplo
+tempo = int(input('Quantos anos tem seu carro?'))
+    if tempo > 3:
+        print('Carro novo')
+        print('--FIM---')
+    else:
+        print('Carro novo')
